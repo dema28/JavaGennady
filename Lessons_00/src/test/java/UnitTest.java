a61bb7beb1ac639052a3b63fba9200e2b32f0783
Denis Novicov <dema28ster@gmail.com>
2025-01-24 21:20:24 +0100

    My first UI test

---
+++ Lessons_00/src/test/java/UnitTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UnitTest {
+
+    @Test
+    void simpeleUnitTest(){
+        Assertions.assertEquals(1, 1);
+
+    }
+}
